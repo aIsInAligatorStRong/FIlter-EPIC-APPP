21eef1e29d8e06e82f5556aeed9832b925426a67
aIsInAligatorStRong <90213463+aIsInAligatorStRong@users.noreply.github.com>
2022-01-16 11:52:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    color="";
+
+}
+function draw() {
+    image(video,0,0,640,480);
+    tint(color);
+}
+function apply() {
+    color=document.getElementById("color").value;
+}
+function take_snapshot() {
+    save('student.png');
+}
